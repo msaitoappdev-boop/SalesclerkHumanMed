176d4452665fccb00eb1738ba82c60006bff411b
msait <msaitoappdev@gmail.com>
2026-04-12 13:04:33 +0900

    問題データを500問に増加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.msaitodev.salesclerk.humanmed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.0"
+        versionCode = 8
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
